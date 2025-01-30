e8b2e2fe7fba549cd901e0a67b7fda54f25ee04d
CerciMuh <147641018+CerciMuh@users.noreply.github.com>
2025-01-30 19:06:45 +0000

    Time: 6 ms (58.47%) | Memory: 21.3 MB (52.05%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,11 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        #hashmap, num in nums will be the index and then give it number 1 and add 1 to it each time and 
+        #retrun top k elements
+        lookup = {}
+        for num in nums: 
+            if num in lookup:
+                lookup[num]+=1
+            else:
+                lookup[num] = 1    
+        return [key for key, _ in sorted(lookup.items(), key=lambda item: item[1], reverse=True)[:k]]
