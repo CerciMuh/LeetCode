546111cc45cd3f7743ed90a2a50b25cb0bce8fa9
CerciMuh <147641018+CerciMuh@users.noreply.github.com>
2025-12-27 12:32:57 +0000

    Time: 1535 ms (8.80%) | Memory: 21.2 MB (13.08%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,28 +1,18 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-    # do for loop to fix a number 
-    # treat other 2 numbers as a 2 sum problem 
-    # regardless of order (im guessing a set) 
-    # will solve later im practically fried today
+        # this will be of O(n) because we will have to loop within all of the list and fix each number entry then do a 2 sum problem with the fixed number to be considered within the summation. before doing all of this we will need to sort the nums to be able to do the 2Sum problem properly 
         nums.sort()
-        result = []
-        for i,num in enumerate(nums):
-            if (i>0 and num == nums [i-1]):
-                 continue
-            l =i+1
-            r= len(nums)-1
-            while(l<r):
-                threeSum = num + nums[l]+nums[r]
-                if threeSum == 0:
-                    result.append([num,nums[l],nums[r]])
+        result = set()
+        for i in  range(len(nums)):
+            l,r = i+1, len(nums)-1
+            while l<r:
+                x = nums[l]+nums[r]+nums[i]
+                if x== 0:
+                    result.add((nums[l],nums[r],nums[i]))
                     l+=1
-                    while (l<r and nums[l] == nums[l-1]):
-
-                        l+=1
-
-                elif threeSum > 0 : 
                     r-=1
-                elif threeSum<0:
-                    l+=1       
-                    
-        return result
+                elif x > 0:
+                    r-=1
+                elif x < 0 :
+                    l+=1
+        return[list(t) for t in result]
