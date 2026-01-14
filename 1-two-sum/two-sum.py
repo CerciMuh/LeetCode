c70c652251efa3628a483a17ab9860d57a1a1e71
CerciMuh <147641018+CerciMuh@users.noreply.github.com>
2026-01-14 22:32:42 +0000

    Time: 1 ms (57.62%) | Memory: 20.4 MB (9.33%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,10 +1,15 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        mp = {} 
-        for i in range(len(nums)):
-            value = nums[i]
-            if value in mp:
-                return [mp[value], i]
-            need = target - value
-            mp[need] = i
-        return []
+        #force solution 
+        hashmap = {}
+        for  i in range(len(nums)):
+            if target - nums[i] in hashmap:
+                return (i,hashmap.get(target-nums[i]))
+            else:
+                hashmap[nums[i]] = i
+
+        #now optimize using a hashmap
+        # each time we visit a number store it in a hashmap 
+        #we start with an empty and start populating the hashmap
+        #one might think u are just rebuilding the list but no hashmap is direct lookup (key,value pair)
+        
