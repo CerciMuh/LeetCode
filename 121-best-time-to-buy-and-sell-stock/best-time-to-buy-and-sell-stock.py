0c728d1611930fc7119d25fa8c7374372be837bc
CerciMuh <147641018+CerciMuh@users.noreply.github.com>
2026-01-14 23:26:32 +0000

    Time: 59 ms (69.96%) | Memory: 28.9 MB (7.40%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,7 +1,7 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         l = 0 
-        r = 0 
+        r = 1
         maxProfit = 0 
         while r < len(prices):
             if prices[r]<prices[l]:
