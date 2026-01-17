bb6ace43f62b9168aa91cb35fa07e6f2a815be8b
CerciMuh <147641018+CerciMuh@users.noreply.github.com>
2026-01-17 20:07:41 +0000

    Time: 13 ms (54.75%) | Memory: 21.7 MB (47.49%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,16 +1,20 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        grouped = {}
-        for string in strs:
-            counts = [0]*26
-            for char in string:
-                counts[ord(char)-ord("a")]+=1
-            key = tuple(counts)
+        # we can go through the list in a for loop 
+        #how can we see if the words have the same letters?
+        #we can set a key value pair
+        #key will be weight of the word and value will be list of words of that weight
+        #so for loop; then a for loop for the letters of the word/ to calculate its weight. 
+        # take the weight and use it as a key, value will be the words of this weight. group key values in list of lists return the list of lists
 
-            if key not in grouped:
-                grouped[key] = []
-            grouped[key].append(string)
+        hashmap = {}
 
-        return list(grouped.values())
-                
+        for word in strs:
+            key = ''.join(sorted(word))
 
+            if key in hashmap:
+                hashmap[key].append(word)
+            else:
+                hashmap[key] = [word]
+
+        return list(hashmap.values())
