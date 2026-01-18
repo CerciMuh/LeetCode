4b0718a8a4be16b515767a62f77ef34e12d27dde
CerciMuh <147641018+CerciMuh@users.noreply.github.com>
2026-01-18 19:10:06 +0000

    Time: 0 ms (100.00%) | Memory: 19.4 MB (6.30%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,15 +1,41 @@
-from collections import deque
-
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = deque()
-        bracket_map = {")": "(", "]": "[", "}": "{"}
+        #opening  = ["(","[","{"]
+        #obviously this has to be recursive
+        #we are checking that for every opening there is a closing
+        #and it is in the correct order
+        #that means there will be some kind of check in the recursive call
+        #I am assuming that all openers will exist then closers
+        #this assumption is quickly debunked because there will exits
+        #()[]
+        #recursive call will be made on a condition?
+        #if str is in opening
+        #then we will call isValid again with the substring of everything after str
+        #if it is a closer then we start comparing?
+        #base case is empty substr?
 
-        for x in s: 
-            if x in bracket_map.values():  
-                stack.append(x)
-            elif x in bracket_map:  
-                if not stack or stack.pop() != bracket_map[x]:
+        #stack attempt
+
+        opening  = ["(","[","{"]
+        closing  = [")","]","}"]
+        pairs = {")":"(",
+                 "]":"[",
+                 "}":"{"}
+
+        stack = deque()
+
+        for bracket in s:
+            if bracket in opening:
+                stack.append(bracket)
+            else:
+                if not stack:
+                    return False
+                openBracket = stack.pop()
+                if openBracket != pairs[bracket]:
                     return False
 
-        return not stack 
+        return not stack
+                
+
+
+        
